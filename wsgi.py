ddc7037f07ca1fbd233bee34f1dd363a4e701e71
notapresent <notapresent@gmail.com>
2015-01-12 22:06:37 +0000

    Run actual webapp

--- wsgi.py
+++ wsgi.py
@@ -12,24 +12,7 @@ except IOError:
 # line, it's possible required libraries won't be in your searchable path
 #
 
-def application(environ, start_response):
-
-    ctype = 'text/plain'
-    if environ['PATH_INFO'] == '/health':
-        response_body = "1"
-    elif environ['PATH_INFO'] == '/env':
-        response_body = ['%s: %s' % (key, value)
-                    for key, value in sorted(environ.items())]
-        response_body = '\n'.join(response_body)
-    else:
-        ctype = 'text/html'
-        response_body = '''<!doctype html><html>Work in progress</html>'''
-
-    status = '200 OK'
-    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
-    #
-    start_response(status, response_headers)
-    return [response_body]
+from rtrss.webapp import app as application
 
 #
 # Below for testing only
