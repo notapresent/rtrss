78e681aee74535e0ba9b2e0724f017bf166b2124
notapresent <notapresent@gmail.com>
2015-01-14 15:20:04 +0300

    Better variable name, fix import

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -2,9 +2,8 @@ import unittest
 from . import MockConfig, DB_URL
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from rtrss.manager import Manager
-from rtrss.dbutil import session_scope, init_db, clear_db
+from rtrss.database import session_scope, init_db, clear_db
 
 
 engine = create_engine(DB_URL, echo=False, client_encoding='utf8')
@@ -26,5 +25,5 @@ class ManagerTestCase(DBTestCase):
     def test_load_topics_returns_empty_(self):
         cfg = MockConfig({'SQLALCHEMY_DATABASE_URI': DB_URL})
         with session_scope(Session) as session:
-            m = Manager(cfg, session)
-            self.assertEqual(m.load_topics([-1]), dict())
+            manager = Manager(cfg, session)
+            self.assertEqual(manager.load_topics([-1]), dict())
