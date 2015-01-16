5421683b33ef6e80d3b410c9d62b5c9cf475f8da
notapresent <notapresent@gmail.com>
2015-01-16 04:14:24 +0300

    Implement BaseDaemon class

---
+++ rtrss/basedaemon.py
@@ -0,0 +1,108 @@
+import daemonized
+import filelock
+import sys
+import signal 
+import os
+import time
+
+
+class BaseDaemon(daemonized.Daemonize):
+    def __init__(self, pidfile, lockfile, stdin=None, stdout=None, stderr=None):
+        self.pidfile = pidfile
+        self._lock = filelock.FileLock(lockfile)
+
+        dev_null = getattr(os, 'devnull', '/dev/null')
+        self.stdin = stdin or dev_null
+        self.stdout = stdout or dev_null
+        self.stderr = stderr or dev_null
+
+    def run(self, *args, **kwargs):
+        raise NotImplementedError('Override this method')
+
+    def start(self):
+        if self.is_running():
+            msg = "Already running\n"
+            sys.stderr.write(msg)
+            sys.exit(1)
+
+        self.make_daemon()
+        
+        try:
+            self._lock.acquire(timeout=1)
+
+        except filelock.Timeout:
+            msg = "Unable to acquire lock on {}\n"
+            sys.stderr.write(msg.format(self.lockfile))
+            sys.exit(1)
+
+        self.run()
+
+    def stop(self):
+        if not self.is_running():
+            return
+
+        pid = self.getpid()
+
+        if not pid:     # Process is running but we are unable to find its id
+            msg = "Can't find process id\n"
+            sys.stderr.write(msg)
+            sys.exit(1)
+
+        self.kill(pid)
+        self.delpid()
+
+    def restart(self):
+            """
+            Restart the daemon
+            """
+            self.stop()
+            self.start()
+
+    def is_running(self):
+        running = False
+
+        try:
+            self._lock.acquire(timeout=1)
+
+        except filelock.Timeout:
+            running = True
+
+        finally:
+            self._lock.release()
+
+        return running
+
+    def getpid(self):
+        try:
+            with open(self.pidfile,'r') as f:
+                pid = int(f.read().strip())
+        except (IOError, ValueError):
+            pid = None
+
+        return pid
+
+    def kill(self, pid, sig=signal.SIGHUP):
+        # Try killing the daemon process
+        try:
+            i = 0
+            while i < 10:
+                os.kill(pid, sig)
+                time.sleep(0.1)
+                i = i + 1
+            os.kill(pid, signal.SIGTERM)
+        except OSError as err:
+            if err.errno == 3:        # No such process
+                pass
+            else:
+                msg = "Failed to kill process {} : {}".format(pid, str(err))
+                sys.stderr.write(msg)
+                sys.exit(1)
+
+    def delpid(self):
+        try:
+            os.remove(self.pidfile)
+        except OSError as err:
+            if err.errno == 2:        # file not found
+                pass
+            sys.stderr.write("Failed to remove pidfile: {}".format(str(err)))
+            sys.exit(1)
