1ee414611fa6e01516d545bb284695a62bd69f0a
notapresent <notapresent@gmail.com>
2015-01-17 06:34:02 +0300

    Change debug action to production

--- rtrss/daemon.py
+++ rtrss/daemon.py
@@ -1,8 +1,8 @@
-import sys
 import os
 import logging
-import atexit
+
 from rtrss.basedaemon import BaseDaemon
+
 
 _logger = logging.getLogger(__name__)
 
@@ -10,10 +10,10 @@ _logger = logging.getLogger(__name__)
 class WorkerDaemon(BaseDaemon):
     def run(self):
         _logger.info('Daemon started ith pid %d', os.getpid())
-        
-        from rtrss.worker import app_init, worker_action
-        worker_action('import_categories') # TODO run()
-        
+
+        from rtrss.worker import worker_action
+        worker_action('run')
+
         _logger.info('Daemon is done and exiting')
 
     def start(self):
@@ -27,7 +27,7 @@ class WorkerDaemon(BaseDaemon):
     def restart(self):
         _logger.info('Restarting daemon')
         super(WorkerDaemon, self).restart()
-    
+
 
 def make_daemon(config):
     '''Returns WorkerDaemon instance'''
@@ -35,4 +35,3 @@ def make_daemon(config):
     logdir = os.environ.get('OPENSHIFT_LOG_DIR') or config.DATA_DIR
     logfile = os.path.join(logdir, 'daemon.log')
     return WorkerDaemon(pidfile, stdout=logfile, stderr=logfile)
-    
