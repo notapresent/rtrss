14d5eeec9667ebb99f95180e0e92c17e1b8eede1
notapresent <notapresent@gmail.com>
2015-01-20 10:04:24 +0000

    Implement rss ttl

--- rtrss/webapp.py
+++ rtrss/webapp.py
@@ -5,12 +5,14 @@ import rfc822
 from flask import Flask, send_from_directory, render_template, make_response
 from flask_sqlalchemy import SQLAlchemy
 from rtrss.models import Topic, User, Category, Torrent
+from rtrss.filestorage import make_storage
 
 app = Flask(__name__)
 app.config.from_object('rtrss.config')
 db = SQLAlchemy(app)
 
-# print app.config['SERVER_NAME']; import sys; sys.exit()
+MIN_TTL = 30    # minutes
+MAX_TTL = 1440  # 1 day
 
 @app.route('/')
 def index():
@@ -22,9 +24,11 @@ def index():
 def torrent(torrent_id):
     pass
 
-@app.route('/feed/')
-def feed():
-    feed_data = get_feed_data()
+# @app.route('/feed/', defaults={'category_id': 0})     
+@app.route('/feed/<int:category_id>')
+def feed(category_id=0):
+    feed_data = get_feed_data(category_id)
+
     content = render_template(
         'feed.xml',
         channel=feed_data['channel'],
@@ -42,13 +46,10 @@ def favicon():
 
 @app.before_first_request
 def setup():
-    pass
-    # Recreate database each time for demo
-    # Base.metadata.drop_all(bind=db.engine)
-    # Base.metadata.create_all(bind=db.engine)
+    pass    # TODO cache warmup etc
 
 
-def get_feed_data(category_id=0):
+def get_feed_data(category_id):
     category = db.session.query(Category).get(category_id)
     channel_data = dict({
         'title': category.title,
@@ -60,17 +61,52 @@ def get_feed_data(category_id=0):
     category_ids = get_subcategories([category_id]) if category_id else None
     topics = get_feed_items(category_ids)
 
+    if not topics:
+        raise RuntimeError('This feed is empty')
+
     items = list()
+    deltas = list()
+    last_dt = None
+
+
     for topic in topics:
         items.append(dict({
             'id': topic.id,
             'title': topic.title,
             'guid': topic.torrent.infohash,
-            'pubDate': datetime_to_rfc822(topic.updated_at)
+            'pubDate': datetime_to_rfc822(topic.updated_at),
         }))
-    # TODO: TTL
+
+        if last_dt:
+            delta = last_dt - topic.updated_at
+            deltas.append(delta.total_seconds())
+
+        last_dt = topic.updated_at
+
+    channel_data['ttl'] = int(calculate_ttl(deltas) / 60)
     return dict({'channel': channel_data, 'items': items})
 
+def calculate_ttl(deltas):
+    """
+    Calculations are based on median time delta between items and the number of
+    items. Accepts a list if time deltas.
+    """
+    def median(lst):
+        lst.sort()
+        length = len(lst)
+        middle = length / 2
+        if length % 2:
+            return lst[middle]
+        else:
+            return (lst[middle - 1] + lst[middle]) / 2.0
+
+    median_delta = median(deltas)
+
+    ttl = median_delta * (len(deltas) + 1) / 2
+
+    ttl = min(max(ttl, MIN_TTL), MAX_TTL)   # Ensure MIN_TTL <= ttl <= MAX_TTL
+    print "Median delta:", median_delta, "Length:", len(deltas) + 1, ' TTL:', str(ttl)
+    return ttl
 
 def datetime_to_rfc822(the_time):
     parsed = rfc822.parsedate_tz(the_time.strftime("%a, %d %b %Y %H:%M:%S"))
@@ -99,48 +135,53 @@ def get_feed_items(category_ids=None):
 
     query = db.session.query(Topic)\
         .join(Torrent)\
-        .order_by(Topic.updated_at.desc())\
-        .limit(limit)
+
 
     if category_ids:
         query = query.filter(Topic.category_id.in_(category_ids))
 
-    return query.all()
+    return query.order_by(Topic.updated_at.desc()).limit(limit).all()
 
 
 def category_link(category, tracker_host):
     if category.is_subforum:
-        return "http://{host}/forum/index.php?c={cid}".format(
-            host=tracker_host, cid=category.id)
-    else:
         return "http://{host}/forum/viewforum.php?f={cid}".format(
-            host=tracker_host, cid=category.id)
-
+            host=tracker_host, cid=category.tracker_id)
+    else:
+        return "http://{host}/forum/index.php?c={cid}".format(
+            host=tracker_host, cid=category.tracker_id)
 
 def make_category_tree():
-    categories = db.session.query(Category)\
-        .filter(Category.skip.isnot(True))\
-        .order_by(Category.parent_id)\
-        .order_by(Category.id)\
-        .all()
+    # TODO show only categories with torrents
+    categories = (
+        db.session.query(Category)
+            .filter(Category.id > 0)
+            .order_by(Category.is_subforum)
+            .order_by(Category.parent_id)
+            .order_by(Category.tracker_id)
+            .all()
+    )
 
-    tree = dict()
-    for cat in categories:
-        if not cat.parent_id:
-            tree[cat.id] = cat
-            continue
+    tree = dict({
+        'id': 0,
+        'title': 'Root',
+        'parent_id': None,
+        'children': list()
+    })
+    childmap = dict({0: tree['children']})
 
-        if cat.parent_id in tree:
-            parent = tree[cat.parent_id]
+    for category in categories:
+        catdict = dict({
+            'id': category.id,
+            'title': category.title,
+            'parent_id': category.parent_id,
+            'children': list()
+        })
 
-            if not parent.get('children'):
-                parent['children'] = list()
+        childmap[category.id] = catdict['children']
+        childmap[category.parent_id].append(catdict)
 
-            parent['children'].append(cat)
-
-        else:
-            raise IndexError("Parent for {} not found".format(cat.id))
-
+    return tree
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 8080, debug=True)
+    app.run(app.config['IP'], app.config['PORT'], debug=True)
