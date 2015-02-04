8211d4b1f2eadbeeaa7064ec1ab040d38f1b3590
notapresent <notapresent@gmail.com>
2015-02-04 23:24:55 +0300

    Emulate DROP TABLES CASCADE

--- rtrss/database.py
+++ rtrss/database.py
@@ -2,15 +2,24 @@ import csv
 import logging
 from contextlib import contextmanager
 
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.engine import reflection
+from sqlalchemy import create_engine
+from sqlalchemy.schema import (
+    MetaData,
+    Table,
+    DropTable,
+    ForeignKeyConstraint,
+    DropConstraint,
+)
 
 from rtrss.exceptions import OperationInterruptedException
 from rtrss import config
 
 
 _logger = logging.getLogger(__name__)
+
 
 engine = create_engine(
     config.SQLALCHEMY_DATABASE_URI,
@@ -56,9 +65,8 @@ def clear(eng=None):
 
     if eng is None:
         eng = engine
-    from rtrss.models import Base
 
-    Base.metadata.drop_all(bind=eng)
+    drop_all_cascade(eng)
 
 
 def import_users(filename):
@@ -87,3 +95,41 @@ def import_users(filename):
             added += 1
 
     _logger.info("%d users added, %d skipped", added, len(lines) - added)
+
+
+def drop_all_cascade(e):
+    conn = e.connect()
+    # the transaction only applies if the DB supports
+    # transactional DDL, i.e. Postgresql, MS SQL Server
+    trans = conn.begin()
+
+    inspector = reflection.Inspector.from_engine(e)
+
+    # gather all data first before dropping anything.
+    # some DBs lock after things have been dropped in
+    # a transaction.
+
+    metadata = MetaData()
+
+    tbs = []
+    all_fks = []
+
+    for table_name in inspector.get_table_names():
+        fks = []
+        for fk in inspector.get_foreign_keys(table_name):
+            if not fk['name']:
+                continue
+            fks.append(
+                ForeignKeyConstraint((), (), name=fk['name'])
+            )
+        t = Table(table_name, metadata, *fks)
+        tbs.append(t)
+        all_fks.extend(fks)
+
+    for fkc in all_fks:
+        conn.execute(DropConstraint(fkc))
+
+    for table in tbs:
+        conn.execute(DropTable(table))
+
+    trans.commit()
