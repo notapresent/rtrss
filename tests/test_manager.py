25ba6d71c0e78ccc71793dd7eb4e50b01c49674e
notapresent <notapresent@gmail.com>
2015-02-06 00:38:17 +0300

    Update parent class

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,8 +1,8 @@
-from . import RTRSSDataBaseTestCase
+from . import DatabaseTestCase
 
 from rtrss import manager
 
 
-class ManagerTestCase(RTRSSDataBaseTestCase):
+class ManagerTestCase(DatabaseTestCase):
     def test_load_topics_returns_empty_(self):
         self.assertEqual(manager.load_topics([-1]), dict())
