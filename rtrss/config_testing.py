68c742c7ced5a74634c1ec0b795737ad3eb2144a
notapresent <notapresent@gmail.com>
2015-02-06 00:39:37 +0300

    Set filestorage url to None by default

--- rtrss/config_testing.py
+++ rtrss/config_testing.py
@@ -1,15 +1,16 @@
 import os
-import tempfile
+
 
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/rtrss_test'
 
-# directory to store runtime data, write access required
-DATA_DIR = tempfile.mkdtemp(dir=ROOT_DIR)
+# This should be set by tests
+DATA_DIR = None
 
 TESTING = True
 
 SECRET_KEY = 'development key'
 
-FILESTORAGE_URL = 'file://{}'.format(DATA_DIR)
+# This should be set by tests
+FILESTORAGE_URL = None
