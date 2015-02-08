37a37aa9f76bf8e735ff4e78203c6efcaac3de91
notapresent <notapresent@gmail.com>
2015-02-08 19:02:33 +0000

    Log webapp creation

--- rtrss/webapp.py
+++ rtrss/webapp.py
@@ -1,4 +1,9 @@
+import logging
+import os
+
 from flask import Flask
+
+_logger = logging.getLogger(__name__)
 
 from rtrss.webapphelpers import db
 from rtrss.views import blueprint
@@ -9,5 +14,5 @@ def make_app(conf):
     app.config.from_object(conf)
     db.init_app(app)
     app.register_blueprint(blueprint)
+    _logger.info('pid:{} Webapp instance created'.format(os.getpid()))
     return app
-
